0078ace529b62a7cecf43a045201f367dda5c204
Fr4nk1in <fushen@mail.ustc.edu.cn>
2022-07-31 03:55:52 +0800

    Adjust Completion Behaviour and UI

--- lua/lsp-config/completion.lua
+++ lua/lsp-config/completion.lua
@@ -12,13 +12,16 @@ cmp.setup({
 			luasnip.lsp_expand(args.body)
 		end,
 	},
+	window = {
+		documentation = cmp.config.window.bordered(),
+	},
 	mapping = cmp.mapping.preset.insert({
 		["<C-d>"] = cmp.mapping.scroll_docs(-4),
 		["<C-f>"] = cmp.mapping.scroll_docs(4),
 		["<C-Space>"] = cmp.mapping.complete(),
 		["<CR>"] = cmp.mapping.confirm({
 			behavior = cmp.ConfirmBehavior.Replace,
-			select = true,
+			select = false,
 		}),
 		["<Tab>"] = cmp.mapping(function(fallback)
 			if cmp.visible() then
