90a1d7456ad5da9d3021c9b9e1fb22fbc9cf5645
Fr4nk1in <fushen@mail.ustc.edu.cn>
2022-08-01 00:30:01 +0800

    Config My Own Startup Window

--- lua/startup-config/init.lua
+++ lua/startup-config/init.lua
@@ -1,1 +1,60 @@
-require("alpha").setup(require("alpha.themes.theta").config)
+local theta = require("alpha.themes.theta")
+local dashboard = require("alpha.themes.dashboard")
+
+local header = {
+	type = "text",
+	val = {
+		[[⠈⠻⢿⣿⣀⠀⠀⠀⠀⠀⠀⠈⠻⣿⣗⢀⠀⠀⠀⠀⠀⠀⠀⠀⠠⢀⡉⠇⠀]],
+		[[⠀⠀⠈⠛⣿⣦⠶⠂⠀⠀⠀⠀⠀⠈⠻⡝⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀]],
+		[[⠀⠀⠀⠀⠘⢾⣷⣄⠀⣀⣀⣠⣤⣄⣤⣌⣀⠀⠀⠀⠀⢀⠀⠀⠀⠀⠁⠀⠀]],
+		[[⠀⠀⠀⠀⠀⠘⣿⣿⣿⣿⣿⣿⣿⣿⣯⣿⡛⢳⣦⡀⡀⠀⠀⠀⠀⠀⠀⠀⠀]],
+		[[⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣼⣴⣿⣿⠂⠀⠀⠀⠀⠀⠀⠀⠀]],
+		[[⠀⡀⠀⠀⠀⢀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⠅⠂⠀⠀⠀⠀⠀⠀⠀]],
+		[[⠄⢛⠀⠀⠀⠘⢻⣿⣿⣿⢿⡽⣿⣿⣿⣿⣿⣿⣿⣿⡧⠆⠀⠀⠀⠀⠀⠀⠀]],
+		[[⠀⢘⡀⠀⠀⠀⠹⣿⠛⠿⠎⢳⣛⣿⣿⣯⣿⣿⣿⠗⢁⠀⠂⠀⡀⠐⡀⠀⠀]],
+		[[⠀⠸⠁⠀⠀⠀⠀⠃⡃⢀⠀⠘⠋⢋⢿⣿⣿⡿⡿⠘⠂⠂⠀⠀⠈⠘⡀⠀⠀]],
+		[[⠀⠀⠇⠀⠀⠀⠀⠈⠀⢀⣀⣤⣴⠟⠸⡻⠟⡙⠀⠐⠀⠀⠀⠀⠦⠨⡀⠠⢠]],
+		[[⠀⠀⠘⡀⠀⠀⠀⠀⠀⠀⠠⠾⡟⠀⠀⠘⣷⠄⠀⠀⠀⠀⠀⠀⠀⠀⢀⠀⠀]],
+		[[⠀⢀⠤⢃⠀⠀⡀⠀⢐⡘⠐⠂⠙⢆⠀⢠⡈⢀⠁⣄⠀⡀⠀⠀⠀⡤⠤⠏⠀]],
+		[[⠀⢨⡅⠚⣀⣀⠄⠀⡁⠀⠠⡀⠄⢠⢷⣾⣕⠁⢰⢋⠥⠀⠁⠄⠀⠐⠒⡁⠀]],
+		[[⠀⠘⡂⠰⢃⡀⠈⠀⠀⠀⠀⠐⡠⠀⠀⢃⠘⠑⡜⠀⠀⠃⢀⠀⠀⠣⠈⠃⠀]],
+		[[⠀⢘⠆⠠⢳⠁⠀⠀⠀⠀⠀⠀⠈⠀⠀⣸⢆⠀⠀⠀⠘⡠⠘⠀⠀⠀⠀⣀⠀]],
+	},
+	opts = {
+		position = "center",
+		hl = "Function",
+		-- wrap = "overflow";
+	},
+}
+
+local more_recent_file = {
+	type = "group",
+	val = {
+		dashboard.button("m", "ﱐ  More Recent Files", "<cmd>Telescope oldfiles<CR>"),
+	},
+	position = "center",
+}
+
+local buttons = {
+	type = "group",
+	val = {
+		{ type = "text", val = "Quick links", opts = { hl = "SpecialComment", position = "center" } },
+		{ type = "padding", val = 1 },
+		dashboard.button("n", "  New file", "<cmd>ene<CR>"),
+		dashboard.button("e", "פּ  File Explorer", "<cmd>NvimTreeToggle<CR>"),
+		dashboard.button("f", "  Find file", "<cmd>Telescope find_files<CR>"),
+		dashboard.button("g", "  Live grep", "<cmd>Telescope live_grep<CR>"),
+		dashboard.button("c", "  Configuration", "<cmd>cd ~/.config/nvim/ <CR>"),
+		dashboard.button("u", "  Update plugins", "<cmd>PackerSync<CR>"),
+		dashboard.button("q", "  Quit", "<cmd>qa<CR>"),
+	},
+	position = "center",
+}
+
+local config = theta.config
+
+config.layout[2] = header
+table.insert(config.layout[4].val, more_recent_file)
+config.layout[6] = buttons
+
+require("alpha").setup(config)
